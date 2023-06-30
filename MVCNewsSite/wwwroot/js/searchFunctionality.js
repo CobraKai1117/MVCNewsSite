155a116fc2e7dbbb0f3b84d0c2478214d1d785a8
Kenneth Paulsen <kennethrpaulsen@gmail.com>
2023-06-30 02:45:08 -0500

    Search Functionality Refactored

---
+++ MVCNewsSite/wwwroot/js/searchFunctionality.js
@@ -0,0 +1,25 @@
+﻿
+    $(document).on('click', '.articleSearchBtn', function () {
+           // window.alert("TESTING1");
+            var query = $('#articleSearch').val();
+    $.get('/News/SearchForArticle', {query: query }, function (data) {
+        $('.ultimate-container').html(data);
+            })
+
+
+        });
+
+
+    $(document).on('keypress','#articleSearch',function(e){
+            if (e.which == 13) {
+                var query = $('#articleSearch').val();
+
+    $.get('/News/SearchForArticle', {query: query }, function (data) {
+        $('.ultimate-container').html(data);
+                })
+
+            }
+
+        })
+
+
